034868ff7bc4866782254bef8089179f6c2b8bda
srivatsa224 <srivatsa224@gmail.com>
2020-03-26 23:07:54 +0530

    Initial commit

---
+++ app/src/main/java/com/srivatsa/splashscreen/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.srivatsa.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Welcome to Mrandroid.in", Toast.LENGTH_SHORT).show()
+
+    }
+}
