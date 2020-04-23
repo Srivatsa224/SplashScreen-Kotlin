611fb12f91dbe8c1f995dddaaf2ba87b109c0d89
srivatsa224 <srivatsa224@gmail.com>
2020-04-23 22:40:30 +0530

    added animation to the splash screen

--- app/src/main/java/com/srivatsa/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/srivatsa/splashscreen/SplashActivity.kt
@@ -4,13 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var image:ImageView
+
+
 
     lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        image=findViewById(R.id.mrandroid)
+        animsplash()
 
         handler= Handler()
         handler.postDelayed({
@@ -19,5 +28,10 @@ class SplashActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         },3000) // 3 seconds delay to open the MainActivity
+    }
+
+    private fun animsplash() {
+    val shake=AnimationUtils.loadAnimation(this,R.anim.shake_anim)
+    image.animation=shake
     }
 }
